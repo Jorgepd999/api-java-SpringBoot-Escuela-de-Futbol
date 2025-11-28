e579bc375f43a1feeb76f37c27c784c5be2042e2
David <david@gmail.com>
2025-11-28 12:56:38 +0000

    Creacion EditCategoriaCommand

--- src/main/java/es/etg/daw/dawes/java/rest/academia/jugadores/application/command/categoria/EditCategoriaCommand.java
+++ src/main/java/es/etg/daw/dawes/java/rest/academia/jugadores/application/command/categoria/EditCategoriaCommand.java
@@ -1,5 +1,18 @@
 package es.etg.daw.dawes.java.rest.academia.jugadores.application.command.categoria;
 
+import es.etg.daw.dawes.java.rest.academia.jugadores.domain.model.categoria.CategoriaId;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Getter
+@AllArgsConstructor
+@Accessors(fluent = true)
 public class EditCategoriaCommand {
+    
+    private final CategoriaId id;
+    private final String nombre;
 
 }
+
+
