bf31552b4fd0ab09b71aa973a6520b39f15a3e68
David <david@gmail.com>
2025-11-28 12:34:06 +0000

    Creacion CategoriaId

---
+++ src/main/java/es/etg/daw/dawes/java/rest/academia/jugadores/domain/model/CategoriaId.java
@@ -0,0 +1,10 @@
+package es.etg.daw.dawes.java.rest.academia.jugadores.domain.model;
+
+import es.etg.daw.dawes.java.rest.academia.common.domain.model.Identificador;
+
+public class CategoriaId extends Identificador {
+    
+    public CategoriaId(Integer value) {
+        super(value);
+    }
+}
