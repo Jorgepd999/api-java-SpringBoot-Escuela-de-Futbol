321d19234137184d2491c5a4d35af21b7052982e
Jorge <jorgeperezduran1999@gmail.com>
2026-01-16 13:20:52 +0100

    Implemtacion logica infraestructura auth y add spring data JPA en el pom

---
+++ src/main/java/es/etg/daw/dawes/java/rest/academia/auth/domain/UsuarioId.java
@@ -0,0 +1,11 @@
+package es.etg.daw.dawes.java.rest.academia.auth.domain;
+
+import es.etg.daw.dawes.java.rest.academia.common.domain.model.Identificador;
+
+public class UsuarioId extends Identificador{
+
+    public UsuarioId(Integer value) {
+        super(value);
+    }
+    
+}
---
+++ src/main/java/es/etg/daw/dawes/java/rest/academia/auth/infraestructure/db/jpa/repository/UserEntityRepository.java
@@ -0,0 +1,11 @@
+package es.etg.daw.dawes.java.rest.academia.auth.infraestructure.db.jpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import es.etg.daw.dawes.java.rest.academia.auth.infraestructure.db.jpa.entity.UserEntity;
+
+@Repository
+public interface UserEntityRepository extends JpaRepository<UserEntity, Integer>{
+    public UserEntity findByEmail(String email);
+}
